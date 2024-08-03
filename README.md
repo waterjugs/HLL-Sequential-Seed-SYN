c6d41db9c8a472010bbb3c77691bd77704d8b791
sbeinart86 <61475834+sbeinart86@users.noreply.github.com>
2024-08-03 12:28:40 -0400

    v1

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# HLL Sequential Seed
- 
+# Install
+
+
+- Download the latest release from the [releases](https://github.com/Craven73/hll-seed/releases/latest) page.
+- Extract the zip file
+- Double click on `enable.bat` to run
+- Click the `More info` button on defender pop up
+> Why are you seeing this pop up? Because am not paying microsoft for a certificate to run this script
+  
+![first](https://github.com/Craven73/hll-seed/assets/50681292/e02530fc-ebca-4e7b-968d-823681db9a2c)
+
+- Click `Run anyway`
+
+![second](https://github.com/Craven73/hll-seed/assets/50681292/bcc3a17d-b388-49db-a4db-46ac89a636a2)
+
+- Repeat these same steps for `script.bat`
+- At this point you should be able to open up your Windows Task Scheduler and see the newly created task `SYN seed`
+
+![task](https://github.com/Craven73/hll-seed/assets/50681292/d57a539e-9eb3-49e5-9e53-5c65ebd34308)
---
+++ script.bat
@@ -0,0 +1,158 @@
+@echo off
+for /f "delims=" %%x in (config.txt) do (set "%%x")
+echo Checking to see if HLL is running...
+set "APPLICATION=HLL-Win64-Shipping.exe"
+echo Launching Seed...
+echo.
+echo Checking Player counts ..
+
+for /f "usebackq delims=," %%i in (`curl -s -X GET %RCON_URLROTN% ^| %JQ_PATH% -r ".result.players.allied"`) do set alliedcountROTN=%%i
+for /f "usebackq delims=," %%i in (`curl -s -X GET %RCON_URLROTN% ^| %JQ_PATH% -r ".result.players.axis"`) do set axiscountROTN=%%i
+
+ 
+echo.Allied Faction has %alliedcountROTN% players
+echo.Axis Faction has %axiscountROTN% players
+for /f "usebackq delims=," %%i in (`curl -s -X GET %RCON_URLROTN% ^| %JQ_PATH% -r ".result.player_count"`) do set countROTN=%%i
+echo.Player Count %countROTN%
+If %countROTN% gtr %SEEDED_THRESHOLD% (
+goto SYNSEED
+)
+if %alliedcountROTN% leq %axiscountROTN% (
+echo Launching as Allies
+ROTNSpawnSL.exe Allied 
+timeout /t 30 >nul
+goto ROTNloop
+) else (
+echo Launching as Axis
+ROTNSpawnSL.exe Axis 
+timeout /t 30 >nul
+
+goto ROTNloop
+)
+
+
+
+:ROTNloop
+
+for /f "usebackq delims=," %%i in (`curl -s -X GET %RCON_URLROTN% ^| %JQ_PATH% -r ".result.player_count"`) do set countROTN=%%i
+
+if %countROTN% gtr %SEEDED_THRESHOLD% (
+    echo Player count is greater than %SEEDED_THRESHOLD%.
+    goto endloop
+) else (
+    echo Player count is %countROTN%. Waiting 30 seconds...
+    timeout /t 30 >nul
+    goto ROTNloop
+)
+
+:endloop
+LeaveGame.exe
+:SYNSEED
+echo.
+
+echo Server is seeded. Onto SYN
+echo Launching Seed...
+echo.
+echo Checking Player counts ..
+
+for /f "usebackq delims=," %%i in (`curl -s -X GET %RCON_URLSYN% ^| %JQ_PATH% -r ".result.players.allied"`) do set alliedcountSYN=%%i
+for /f "usebackq delims=," %%i in (`curl -s -X GET %RCON_URLSYN% ^| %JQ_PATH% -r ".result.players.axis"`) do set axiscountSYN=%%i
+
+ 
+echo.Allied Faction has %alliedcountSYN% players
+echo.Axis Faction has %axiscountSYN% players
+for /f "usebackq delims=," %%i in (`curl -s -X GET %RCON_URLSYN% ^| %JQ_PATH% -r ".result.player_count"`) do set countSYN=%%i
+echo.Player Count %countSYN%
+If %countSYN% gtr %SEEDED_THRESHOLD% (
+goto CTRLSEED
+)
+
+if %alliedcountSYN% leq %axiscountSYN% (
+echo Launching as Allies
+SYNSpawnSL.exe Allied 
+timeout /t 30 >nul
+goto SYNloop
+) else (
+echo Launching as Axis
+SYNSpawnSL.exe Axis 
+timeout /t 30 >nul
+
+goto SYNloop
+)
+
+
+
+:SYNloop
+
+for /f "usebackq delims=," %%i in (`curl -s -X GET %RCON_URLSYN% ^| %JQ_PATH% -r ".result.player_count"`) do set countSYN=%%i
+
+if %countSYN% gtr %SEEDED_THRESHOLD% (
+    echo Player count is greater than %SEEDED_THRESHOLD%.
+    goto endloop
+) else (
+    echo Player count is %countSYN%. Waiting 30 seconds...
+    timeout /t 30 >nul
+    goto SYNloop
+)
+
+:endloop
+LeaveGame.exe
+:CTRLSEED
+echo Server is seeded. Onto CTRL
+echo Launching Seed...
+echo.
+echo Checking Player counts ..
+
+for /f "usebackq delims=," %%i in (`curl -s -X GET %RCON_URLCTRL% ^| %JQ_PATH% -r ".result.players.allied"`) do set alliedcountCTRL=%%i
+for /f "usebackq delims=," %%i in (`curl -s -X GET %RCON_URLCTRL% ^| %JQ_PATH% -r ".result.players.axis"`) do set axiscountCTRL=%%i
+
+ 
+echo.Allied Faction has %alliedcountCTRL% players
+echo.Axis Faction has %axiscountCTRL% players
+for /f "usebackq delims=," %%i in (`curl -s -X GET %RCON_URLCTRL% ^| %JQ_PATH% -r ".result.player_count"`) do set countCTRL=%%i
+echo.Player Count %countCTRL%
+If %countCTRL% gtr %SEEDED_THRESHOLD% (
+goto endloop
+)
+
+if %alliedcountCTRL% leq %axiscountCTRL% (
+echo Launching as Allies
+CTRLSpawnSL.exe Allied 
+timeout /t 30 >nul
+goto CTRLloop
+) else (
+echo Launching as Axis
+CTRLSpawnSL.exe Axis 
+timeout /t 30 >nul
+
+goto CTRLloop
+)
+)
+
+
+:CTRLloop
+
+for /f "usebackq delims=," %%i in (`curl -s -X GET %RCON_URL_CTRL% ^| %JQ_PATH% -r ".result.player_count"`) do set countCTRL=%%i
+
+if %countCTRL% gtr %SEEDED_THRESHOLD% (
+    echo Player count is greater than %SEEDED_THRESHOLD%.
+    goto endloop
+) else (
+    echo Player count is %countCTRL%. Waiting 30 seconds...
+    timeout /t 30 >nul
+    goto SYNloop
+)
+
+:endloop
+
+TASKKILL /IM HLL-Win64-Shipping.exe /F
+
+echo Waiting for HLL to finish closing
+timeout /t 60 >nul
+
+echo Putting the PC to sleep...
+REM powercfg -h off
+REM rundll32.exe powrprof.dll,SetSuspendState 0,1,0
+REM powercfg -h on
+
+echo PC is now asleep.
